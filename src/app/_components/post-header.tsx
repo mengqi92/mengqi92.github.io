c2c3ac3c1abb1c9326379032f42d312a19941ddb
Mengqi Pei <mengqipei@gmail.com>
2024-03-31 16:35:05 +0800

    Put tags in the same line as date

--- src/app/_components/post-header.tsx
+++ src/app/_components/post-header.tsx
@@ -14,19 +14,19 @@ export function PostHeader({ post }: Props) {
     <>
       <PostTitle>{post.title}</PostTitle>
       <div className="flex flex-col justify-between text-sm text-gray-500 dark:text-gray-400">
-        <div className="flex items-center space-x-4">
+        <div className="flex justify-between items-center space-x-4">
           <div className="italic">
             <DateFormatter dateString={post.createdDate} />
           </div>
-        </div>
-        <div className="flex flex-wrap items-center gap-1 mt-2 text-red-500">
-          {post.tags && post.tags.map(t => (
-            <div key={t} className="hover:text-red-700 dark:bg-red-800 rounded-full px-1 py-1 dark:text-red-300">
-              <Link href="/tags/[tag]" as={`/tags/${encodeURIComponent(t)}`}>
-                #{t}
-              </Link>
-            </div>
-          ))}
+          <div className="flex flex-wrap items-center gap-1 text-red-500">
+            {post.tags && post.tags.map(t => (
+              <div key={t} className="hover:text-red-700 dark:bg-red-800 rounded-full px-1 py-1 dark:text-red-300">
+                <Link href="/tags/[tag]" as={`/tags/${encodeURIComponent(t)}`}>
+                  #{t}
+                </Link>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
       <hr className="mt-4 mb-10 border-gray-200 dark:border-gray-700" />
