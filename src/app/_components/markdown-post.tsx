15d092ac7664886d36c2a654a57255812a998c0c
Mengqi Pei <mengqipei@gmail.com>
2024-03-31 17:28:00 +0800

    Polish table UI

--- src/app/_components/markdown-post.tsx
+++ src/app/_components/markdown-post.tsx
@@ -7,7 +7,7 @@ const components: MDXComponents = {
     h1: ({ children }) => <h1 className="my-4 scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl [&:not(:first-child)]:mt-8">{children}</h1>,
     h2: ({ children }) => <h2 className="my-4 scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0 [&:not(:first-child)]:mt-8">{children}</h2>,
     h3: ({ children }) => <h3 className="my-4 scroll-m-20 text-2xl font-semibold tracking-tight [&:not(:first-child)]:mt-8">{children}</h3>,
-    h4: ({ children }) => <h4 className="my-4 scroll-m-20 text-xl font-semibold tracking-tight[&:not(:first-child)]: mt-4">{children}</h4>,
+    h4: ({ children }) => <h4 className="my-4 scroll-m-20 text-xl font-semibold tracking-tight [&:not(:first-child)]:mt-4">{children}</h4>,
     p: ({ children }) => <p className="leading-7 text-lg [&:not(:first-child)]:mt-6">{children}</p>,
     blockquote: ({ children }) => <blockquote className="mt-6 bg-slate-50 py-2 border-l-2 pl-6 italic">{children}</blockquote>,
     ul: ({ children }) => <ul className="my-6 ml-6 list-disc [&>li]:mt-2">{children}</ul>,
@@ -23,7 +23,13 @@ const components: MDXComponents = {
     lead: ({ children }) => <p className="text-xl text-muted-foreground">{children}</p>,
     a: ({ children, href }) => (
         <a href={href} className="text-red-500 underline decoration-red-400 underline-offset-4 hover:text-red-700 hover:decoration-red-700 hover:decoration-2">{children}</a>
-    )
+    ),
+    table: ({children}) => <table className="table-auto my-2 w-full text-sm text-right rtl:text-left text-gray-500 dark:text-gray-400">{children}</table>,
+    tbody: ({children}) => <tbody className="bg-slate-100 dark:bg-white-700 text-gray-700 dark:text-gray-400">{children}</tbody>,
+    thead: ({children}) => <thead className="text-gray-700 uppercase bg-sky-50 dark:bg-gray-700 dark:text-gray-400 border-b-2 border-b-gray-500">{children}</thead>,
+    th: ({children}) => <th className="px-4 py-3">{children}</th>,
+    td: ({children}) => <td className="px-4 py-4">{children}</td>,
+    tr: ({children}) => <tr className="divide-y [&:not(:last-child)]:border-b-2 border-b-slate-200">{children}</tr>
 };
 
 interface MarkdownPostProps {
