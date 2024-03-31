c7e3a49f4114c8920970b17e27277e82303e60fa
Mengqi Pei <mengqipei@gmail.com>
2024-03-31 16:16:48 +0800

    Polish markdown font

--- src/app/_components/markdown-post.tsx
+++ src/app/_components/markdown-post.tsx
@@ -4,11 +4,11 @@ import clsx from "clsx";
 
 const components: MDXComponents = {
     // Allows customizing built-in components, e.g. to add styling.
-    h1: ({ children }) => <h1 className="my-4 scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">{children}</h1>,
-    h2: ({ children }) => <h2 className="my-4 scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">{children}</h2>,
-    h3: ({ children }) => <h3 className="my-4 scroll-m-20 text-2xl font-semibold tracking-tight">{children}</h3>,
-    h4: ({ children }) => <h4 className="my-4 scroll-m-20 text-xl font-semibold tracking-tight">{children}</h4>,
-    p: ({ children }) => <p className="leading-7 [&:not(:first-child)]:mt-6">{children}</p>,
+    h1: ({ children }) => <h1 className="my-4 scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl [&:not(:first-child)]:mt-8">{children}</h1>,
+    h2: ({ children }) => <h2 className="my-4 scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0 [&:not(:first-child)]:mt-8">{children}</h2>,
+    h3: ({ children }) => <h3 className="my-4 scroll-m-20 text-2xl font-semibold tracking-tight [&:not(:first-child)]:mt-8">{children}</h3>,
+    h4: ({ children }) => <h4 className="my-4 scroll-m-20 text-xl font-semibold tracking-tight[&:not(:first-child)]: mt-4">{children}</h4>,
+    p: ({ children }) => <p className="leading-7 text-lg [&:not(:first-child)]:mt-6">{children}</p>,
     blockquote: ({ children }) => <blockquote className="mt-6 border-l-2 pl-6 italic">{children}</blockquote>,
     ul: ({ children }) => <ul className="my-6 ml-6 list-disc [&>li]:mt-2">{children}</ul>,
     li: ({ children }) => <li className="leading-7 list-outside list-decimal">{children}</li>,
