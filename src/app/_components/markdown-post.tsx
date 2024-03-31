ecc28dffebff8e8b5703cda394b375f1345d8ee3
Mengqi Pei <mengqipei@gmail.com>
2024-03-31 16:35:31 +0800

    Add background to blockquote

--- src/app/_components/markdown-post.tsx
+++ src/app/_components/markdown-post.tsx
@@ -9,7 +9,7 @@ const components: MDXComponents = {
     h3: ({ children }) => <h3 className="my-4 scroll-m-20 text-2xl font-semibold tracking-tight [&:not(:first-child)]:mt-8">{children}</h3>,
     h4: ({ children }) => <h4 className="my-4 scroll-m-20 text-xl font-semibold tracking-tight[&:not(:first-child)]: mt-4">{children}</h4>,
     p: ({ children }) => <p className="leading-7 text-lg [&:not(:first-child)]:mt-6">{children}</p>,
-    blockquote: ({ children }) => <blockquote className="mt-6 border-l-2 pl-6 italic">{children}</blockquote>,
+    blockquote: ({ children }) => <blockquote className="mt-6 bg-slate-50 py-2 border-l-2 pl-6 italic">{children}</blockquote>,
     ul: ({ children }) => <ul className="my-6 ml-6 list-disc [&>li]:mt-2">{children}</ul>,
     li: ({ children }) => <li className="leading-7 list-outside list-decimal">{children}</li>,
     pre: ({ className, children, ...props }) => {
