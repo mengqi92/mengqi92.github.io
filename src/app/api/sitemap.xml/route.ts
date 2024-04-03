7ea034dfdb2d85bd23a493d922eeeefe5079d905
Mengqi Pei <mengqipei@gmail.com>
2024-04-03 20:01:25 +0800

    Add legacy pages to sitemap

--- src/app/api/sitemap.xml/route.ts
+++ src/app/api/sitemap.xml/route.ts
@@ -21,7 +21,38 @@ export async function GET(request: Request) {
         '!api',
     ])
     console.table(pages);
-    const fields = pages.map(addPage);
+    let fields = pages.map(addPage);
+    fields = fields.concat(legacyPageMap())
 
     return getServerSideSitemap(fields);
+}
+
+const legacyPages = [{ year: "2016", month: "06", day: "23", slug: "wishful-thinking" },
+    { year: "2021", month: "02", day: "23", slug: "personal-finance-101-xnpv" },
+    { year: "2020", month: "07", day: "19", slug: "personal-finance-101-rate-of-return" },
+    { year: "2015", month: "10", day: "05", slug: "logistic-regression" },
+    { year: "2016", month: "06", day: "22", slug: "linear-algebra-5" },
+    { year: "2016", month: "06", day: "20", slug: "linear-algebra-4" },
+    { year: "2016", month: "07", day: "01", slug: "linear-algebra-6" },
+    { year: "2016", month: "05", day: "20", slug: "linear-algebra-3" },
+    { year: "2016", month: "05", day: "03", slug: "linear-algebra-1" },
+    { year: "2016", month: "05", day: "14", slug: "linear-algebra-2" },
+    { year: "2020", month: "07", day: "17", slug: "hide-files-from-git" },
+    { year: "2018", month: "09", day: "18", slug: "git-housekeeping" },
+    { year: "2018", month: "05", day: "09", slug: "haskell-notes-1-basic-and-typeclass" },
+    { year: "2015", month: "10", day: "11", slug: "gabor" },
+    { year: "2015", month: "10", day: "06", slug: "convolution" },
+    { year: "2015", month: "10", day: "13", slug: "Ein-Eout" },
+    { year: "2015", month: "10", day: "06", slug: "complex" },
+    { year: "2015", month: "10", day: "03", slug: "think-statistics-note" }]
+
+function legacyPageMap(): ISitemapField[] {
+    return legacyPages.map((({year, month, day, slug}) => {
+        return {
+            loc: `https://mengqi92.github.io/${year}/${month}/${day}/${slug}`,
+            lastmod: new Date().toISOString(),
+            changefreq: 'yearly',
+            priority: 1.0
+        }
+    }))
 }
