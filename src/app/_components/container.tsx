1e6b84c1f58660882528e7a6d2ce5d1dcf2c8ed3
Mengqi Pei <mengqipei@gmail.com>
2024-04-03 20:44:05 +0800

    Enlarge container padding

--- src/app/_components/container.tsx
+++ src/app/_components/container.tsx
@@ -3,7 +3,7 @@ type Props = {
 };
 
 const Container = ({ children }: Props) => {
-  return <div className="container mx-auto px-1">{children}</div>;
+  return <div className="container mx-auto px-6 sm:px-6 lg:px-8">{children}</div>;
 };
 
 export default Container;
