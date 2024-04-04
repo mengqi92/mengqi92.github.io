b2704473f239a86fc1379a2eecb5fc0476948eb7
Mengqi Pei <mengqipei@gmail.com>
2024-04-04 14:59:21 +0800

    Remove underline class

--- src/app/tags/[tag]/page.tsx
+++ src/app/tags/[tag]/page.tsx
@@ -20,7 +20,7 @@ export default function page({ params }: { params: { tag: string } }) {
     return (
         <>
             <div className="container mx-auto max-w-3xl md:px-6 py-8">
-                <h1 className="text-4xl font-bold mb-8 underline decoration-red-500">
+                <h1 className="text-4xl font-bold mb-8">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mr-1">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M9.568 3H5.25A2.25 2.25 0 0 0 3 5.25v4.318c0 .597.237 1.17.659 1.591l9.581 9.581c.699.699 1.78.872 2.607.33a18.095 18.095 0 0 0 5.223-5.223c.542-.827.369-1.908-.33-2.607L11.16 3.66A2.25 2.25 0 0 0 9.568 3Z" />
                         <path strokeLinecap="round" strokeLinejoin="round" d="M6 6h.008v.008H6V6Z" />
