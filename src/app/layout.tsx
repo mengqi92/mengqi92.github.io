fa06d7a565db204aea86cbedef898db0a0272149
Mengqi Pei <mengqipei@gmail.com>
2024-05-04 18:06:48 +0800

    Add google adsense verify script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="light">
       <head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8258722689100385" crossOrigin="anonymous"></script>
         <GoogleAnalytics />
         <Clarity />
         <link
